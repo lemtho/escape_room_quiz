38a3be19343765ab9235f956f798dd0cb0a422ef
nchan5 <44488011+nchan5@users.noreply.github.com>
2020-04-14 00:10:36 -0400

    Initialize web application

---
+++ server.js
@@ -0,0 +1,24 @@
+// Import modules and initialize.
+var express = require("express");
+
+// Create the express application.
+var app = express();
+
+// Set the port. Have Heroku select port or use port 22580.
+var port = process.env.PORT || 22580
+
+// When request to app is received to also allow server to serve static files, such as
+// HTML, CSS, and front-end JS files.
+app.use(express.static(__dirname + "/public"));
+
+/* ESTABLISH ROUTES. */
+
+// TEST: Render static index.html file when user wants to access the home page.
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.listen(port, function()
+{
+    console.log("Express is running on port " + port + ".\n");
+});
