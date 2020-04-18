68d02e9b0111b464f657ed88e718c3528a78bab6
nchan5 <44488011+nchan5@users.noreply.github.com>
2020-04-17 21:08:43 -0400

    Added comments and made syntax consistent

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 // Create the express application.
 var app = express();
 
+// Set the engine used to render the (front-end) web pages.
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -13,44 +14,46 @@ var port = process.env.PORT || 22580
 
 /* ESTABLISH ROUTES AND THE JS FILE THE ENGINE SHOULD USE WHEN PROCESSING THE ROUTE. */
 
-// IF route to home page, run command in homePage.js
-app.use("/", express.static('public'));
-app.use("/", require('./homePage.js'));
+/* IF route to home page, execute homePage.js script. Additionally, for this and the
+following routes listed below grab static images, CSS scripts, and/or front-end JS scripts 
+from the "public" folder. */
+app.use("/", express.static("public"));
+app.use("/", require("./homePage.js"));
 
-// IF route to student home page, run command in studentHomePage.js
-app.use("/studentHomePage", express.static('public'));
-app.use("/studentHomePage", require('./studentHomePage.js'));
+// IF route to student home page, execute studentHomePage.js script.
+app.use("/studentHomePage", express.static("public"));
+app.use("/studentHomePage", require("./studentHomePage.js"));
 
-// IF route to student scoreboard page, run command in studentScoreboard.js
-app.use("/studentScoreboard", express.static('public'));
-app.use("/studentScoreboard", require('./studentScoreboard.js'));
+// IF route to student scoreboard page, execute studentScoreboard.js script.
+app.use("/studentScoreboard", express.static("public"));
+app.use("/studentScoreboard", require("./studentScoreboard.js"));
 
-// IF route to student profile, run commands in studentProfile.js
-app.use("/studentProfile", express.static('public'));
-app.use("/studentProfile", require('./studentProfile.js'));
+// IF route to student profile, execute studentProfile.js script.
+app.use("/studentProfile", express.static("public"));
+app.use("/studentProfile", require("./studentProfile.js"));
 
-// IF route to teacher home page, run command in teacherHomePage.js
-app.use("/teacherHomePage", express.static('public'));
-app.use("/teacherHomePage", require('./teacherHomePage.js'));
+// IF route to teacher home page, execute teacherHomePage.js script.
+app.use("/teacherHomePage", express.static("public"));
+app.use("/teacherHomePage", require("./teacherHomePage.js"));
 
 // IF route to teacher quiz page, execute teacherQuiz.js script.
-app.use("/teacherQuiz", express.static('public'));
-app.use("/teacherQuiz", require('./teacherQuiz.js'));
+app.use("/teacherQuiz", express.static("public"));
+app.use("/teacherQuiz", require("./teacherQuiz.js"));
 
 // IF route to teacher student page, execute teacherStudent.js script.
-app.use("/teacherStudents", express.static('public'));
-app.use("/teacherStudents", require('./teacherStudents.js'));
+app.use("/teacherStudents", express.static("public"));
+app.use("/teacherStudents", require("./teacherStudents.js"));
 
 // IF route to teacher scoreboard, execute teacherScoreboard.js script.
-app.use("/teacherScoreboard", express.static('public'));
-app.use("/teacherScoreboard", require('./teacherScoreboard.js'));
+app.use("/teacherScoreboard", express.static("public"));
+app.use("/teacherScoreboard", require("./teacherScoreboard.js"));
 
 // IF route to teacher profile page, execute teacherProfile.js script.
-app.use("/teacherProfile", express.static('public'));
-app.use("/teacherProfile", require('./teacherProfile.js'));
+app.use("/teacherProfile", express.static("public"));
+app.use("/teacherProfile", require("./teacherProfile.js"));
 
 // IF route to a page that is not found, render a HTTP 404 response.
-app.use(function(req,res)
+app.use(function(req, res)
 {
     res.status(404);
     res.render("pageNotFound");
@@ -64,6 +67,7 @@ app.use(function(err, req, res, next)
     res.render("serverError");
 });
 
+// Have the app (i.e., server) output message notifying user it is listening for HTTP requests on the port.
 app.listen(port, function()
 {
     console.log("Express is running on port " + port + ". Press Ctrl+C to terminate the session.\n");
