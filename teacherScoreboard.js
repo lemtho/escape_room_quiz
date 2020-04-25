9b6b0304b2fe58918573b917abb184aba46cebb0
Thomas Lem <lemt@oregonstate.edu>
2020-04-25 12:03:44 -0700

    working on filter

--- teacherScoreboard.js
+++ teacherScoreboard.js
@@ -18,6 +18,68 @@ module.exports = function()
 		});
 	}
 
+	function getQuizDrop(res, mysql, id, context, complete) {
+		var sql = "SELECT q.quizID, q.name FROM quiz AS q JOIN teacher AS t ON q.teacherID = t.teacherID WHERE t.teacherID = ? ORDER BY name ASC;";
+		var inserts = [id];
+		mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if (error) {
+				console.log(error);
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+
+			context.quizDrop = results;
+			complete();
+		});
+	}
+
+	function getStudentDrop(res, mysql, id, context, complete) {
+		var sql = "SELECT DISTINCT s.studentID, s.lastName, s.firstName FROM teacher AS t LEFT JOIN quiz AS q ON t.teacherID = q.teacherID JOIN (SELECT studentID, quizID FROM student_question GROUP BY studentID, quizID) sq USING (quizID) JOIN student AS s ON sq.studentID = s.studentID WHERE t.teacherID = ? ORDER BY lastName ASC;";
+		var inserts = [id];
+		mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if (error) {
+				console.log(error);
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+
+			context.studentDrop = results;
+			complete();
+		});
+	}
+
+	function getQuizScores(res, mysql, id, context, complete) {
+		var sql = "SELECT DISTINCT s.studentID, s.lastName, s.firstName, actual_score, q.quizID FROM teacher AS t LEFT JOIN quiz AS q ON t.teacherID = q.teacherID JOIN (SELECT studentID, quizID, SUM(questionPT) as actual_score FROM student_question GROUP BY studentID, quizID) sq USING (quizID) JOIN student AS s ON sq.studentID = s.studentID WHERE t.teacherID = ? ORDER BY lastName ASC;";
+		var inserts = [id];
+		mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if (error) {
+				console.log(error);
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+
+			context.quizScore = results;
+			complete();
+		});
+	}
+
+	function getStudentScores(res, mysql, id, context, complete) {
+		var sql = "SELECT DISTINCT s.studentID, q.quizID, q.name, actual_score FROM teacher AS t LEFT JOIN quiz AS q ON t.teacherID = q.teacherID JOIN (SELECT studentID, quizID, SUM(questionPT) as actual_score FROM student_question GROUP BY studentID, quizID) sq USING (quizID) JOIN student AS s ON sq.studentID = s.studentID WHERE t.teacherID = ? ORDER BY name ASC;";
+		var inserts = [id];
+		mysql.pool.query(sql, inserts, function(error, results, fields) {
+			if (error) {
+				console.log(error);
+				res.write(JSON.stringify(error));
+				res.end();
+			}
+
+			context.studentScore = results;
+			complete();
+		});
+	}
+
+	
+
 	function getStudentResults(res, mysql, qid, sid, context, complete) {
 		var sql = "SELECT s.firstName, s.lastName, quest.questionID, quest.question, quest.answer, studentAnswer, questionPt, q.name FROM student AS s LEFT JOIN (SELECT questionID, studentID, studentAnswer, questionPT FROM student_question WHERE quizID = ?) sq USING (studentID) JOIN question AS quest ON sq.questionID = quest.questionID JOIN quiz AS q ON quest.quizID = q.quizID WHERE studentID = ?;";
 		var inserts = [qid, sid];
@@ -71,10 +133,37 @@ module.exports = function()
 			var context = {};
 			var mysql = req.app.get('mysql');
 			getScores(res, mysql, teacherID, context, complete);
+			getQuizDrop(res, mysql, teacherID, context, complete);
+			getStudentDrop(res, mysql, teacherID, context, complete);
+			getQuizScores(res, mysql, teacherID, context, complete);
+			getStudentScores(res, mysql, teacherID, context, complete);
 
 			function complete() {
 				callbackCount++;
-				if (callbackCount >= 1) {
+				if (callbackCount >= 5) {
+					context.title = "Scoreboard";
+					res.render('teacherScoreboard', context);
+				}
+			}
+		}
+		else {
+			res.redirect("/");
+		}
+	});
+
+	router.get("/", function(req, res){
+		if (req.session.teacherID) {
+			var teacherID = req.session.teacherID;
+			var callbackCount = 0;
+			var context = {};
+			var mysql = req.app.get('mysql');
+			getScores(res, mysql, teacherID, context, complete);
+			getQuizDrop(res, mysql, teacherID, context, complete);
+			getStudentDrop(res, mysql, teacherID, context, complete);
+
+			function complete() {
+				callbackCount++;
+				if (callbackCount >= 3) {
 					context.title = "Scoreboard";
 					res.render('teacherScoreboard', context);
 				}
