0acc842decfffda2ae002b022822a0dd153eb819
nchan5 <channe@oregonstate.edu>
2020-04-25 21:18:19 -0400

    Fixed relative position viewing

--- public/frontendJS/studentHomePage.js
+++ public/frontendJS/studentHomePage.js
@@ -7,10 +7,6 @@ document.getElementById("goButton").addEventListener("click", function(appear)
     // Hide the greeting and form divs.
     document.getElementById("studentHomeDiv3").style.display = "none";
     document.getElementById("studentHomeDiv4").style.display = "none";
-
-    // Increase height of page div.
-    document.getElementById("studentHomeDiv1").style.height = "825px";
-    document.getElementById("studentHomeDiv2").style.height = "825px";
     
     // Display the game.
     document.getElementById("game").style.display = "block";
