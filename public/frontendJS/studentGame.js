6ad68c35ee000f56986424b22515f909b1e2a528
Nelson K. Chan <channe@oregonstate.edu>
2020-05-06 23:05:44 -0400

    Added student game route

---
+++ public/frontendJS/studentGame.js
@@ -0,0 +1,3 @@
+// On load hide the TEST <p> elements.
+document.getElementById("studentID").style.display = "none";
+document.getElementById("quizID").style.display = "none";
--- studentHomePage.js
+++ studentHomePage.js
@@ -27,6 +27,65 @@ module.exports = function()
 			res.redirect("/");
 		}
 	});
+
+	// On studentHomePage/playGame load...
+	router.get("/playGame", function(req, res){
+		
+		// TEST: Output session data.
+		// console.log(req.session);
+		
+		// IF user is already signed in and quiz ID is valid...
+		if (req.session.studentID != "" & req.session.quizID != "")
+		{
+			/* Declare a local variable that stores the user's id and user's first name obtained from 
+			session data. User's first name will be displayed on page. */
+			var studentID = req.session.studentID;
+			var quizID = req.session.quizID;
+		
+			res.render("studentGame", {title: "Student Game Page", studentID: studentID, quizID: quizID});
+		}
+
+		// ELSE user is not signed in or have valid quiz code...
+		else
+		{
+			// Redirect user to home page.
+			res.redirect("/");
+		}
+	});
+
+	// IF user hits "Go" button to play the game...
+	router.post("/playGame", function(req, res){
+
+		var mysql = req.app.get("mysql");
+		
+		// TEST: Output to console the quiz code user inputted.
+		// console.log(req.body);
+
+		/* Declare a local variable that stores the user's id and 
+		a local variable that stores the quiz id. */
+		var quizID = req.body.quizID;
+
+		// Check the database whether quizID exists.
+		mysql.pool.query("SELECT `name` FROM `quiz` WHERE `quizID` = ?", [quizID], function(err, results, fields)
+		{
+			// IF query returned a result...
+			if (results.length > 0)
+			{
+				// Store the quiz ID in sessions data.
+				req.session.quizID = quizID;
+
+				// Redirect user to student game page.
+				res.redirect("/studentHomePage/playGame");
+			}
+
+			/* ELSE query did not return a reuslt. Send a HTTP 400 status code and error message
+			to client. */
+			else
+			{
+				res.status(400).send("Invalid quiz code! Please try again.")
+			} 
+		});
+	});
 	
 	return router;
 }();
