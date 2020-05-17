fefe439359907a0a0bf2e848fd298db5edea1b80
Nelson K. Chan <channe@oregonstate.edu>
2020-05-16 22:57:43 -0400

    Added logic to check whether student took quiz on game load and added command to convert quiz code to quiz ID

--- studentHomePage.js
+++ studentHomePage.js
@@ -55,7 +55,7 @@ module.exports = function()
 
 			/* Query the database for the questions found under the quizID and store the results 
 			inside context object. */
-			mysql.pool.query("SELECT * from question WHERE quizID = ?", [quizID], function(error, results, fields)
+			mysql.pool.query("SELECT * from question WHERE quizID = ?;", [quizID], function(error, results, fields)
 			{
 				if (error)
 				{
@@ -70,7 +70,7 @@ module.exports = function()
 			});
 		}
 
-		// ELSE user is not signed in or have valid quiz code...
+		// ELSE user is not signed in, does not have valid quiz code, or took the quiz already...
 		else
 		{
 			// Redirect user to home page.
@@ -87,20 +87,38 @@ module.exports = function()
 		// console.log(req.body);
 
 		/* Declare a local variable that stores the user's id and 
-		a local variable that stores the quiz id. */
+		a local variable that stores the quiz code user inputted. */
 		var quizID = req.body.quizID;
+		var studentID = req.session.studentID;
+
+		// Convert quiz code to quiz ID.
+		quizID = (quizID - 89) / 273;
 
 		// Check the database whether quizID exists.
-		mysql.pool.query("SELECT `name` FROM `quiz` WHERE `quizID` = ?", [quizID], function(err, results, fields)
+		mysql.pool.query("SELECT `name` FROM `quiz` WHERE `quizID` = ?;", [quizID], function(err, results, fields)
 		{
 			// IF query returned a result...
 			if (results.length > 0)
 			{
-				// Store the quiz ID in sessions data.
-				req.session.quizID = quizID;
+				// Check whether student took the quiz.
+				mysql.pool.query("SELECT * FROM student_question WHERE quizID = ? AND studentID = ?;", [quizID, studentID], function(err, rows, fields)
+				{
+					// IF student did not take the quiz...
+					if (rows.length <= 0)
+					{
+						// Store the quiz ID in sessions data.
+						req.session.quizID = quizID;
 
-				// Redirect user to student game page.
-				res.redirect("/studentHomePage/playGame");
+						// Redirect user to student game page.
+						res.redirect("/studentHomePage/playGame");
+					}
+
+					// ELSE, student took the quiz. Send a HTTP 400 status code and error message to client.
+					else
+					{
+						res.status(400).send("Error! It appears you took this quiz already. Please ask your teacher if you have any questions about this quiz.");
+					}
+				});
 			}
 
 			/* ELSE, query did not return a result. Send a HTTP 400 status code and error message
@@ -118,7 +136,7 @@ module.exports = function()
 		var mysql = req.app.get("mysql");
 		
 		// TEST: Output to console data received from the game.
-		console.log(req.body);
+		// console.log(req.body);
 
 		/* Declare and store values from the request to these local variables that will
 		be referenced in the query to send to the database. */
@@ -128,7 +146,7 @@ module.exports = function()
 		var questionPT = req.body.questionPT;
 		var dateTaken = req.body.dateTaken;
 		var quizID = req.body.quizID;
-
+		
 		/* Create a query to INSERT a record into the student_question table in the database
 		with the following values extracted from the request. */
 		mysql.pool.query("INSERT INTO student_question VALUES (?, ?, ?, ?, ?, ?);", [studentID, questionID, studentAnswer, questionPT, dateTaken, quizID], function(error, results, fields)
@@ -139,6 +157,16 @@ module.exports = function()
 				// TEST: Output error message in console.
 				// console.log(JSON.stringify(error));
 				res.end();
+			}
+
+			// IF quizID in session data has not been reset to 0...
+			if (req.session.quizID > 0)
+			{
+				// Reset quizID stored in session data to 0 so user cannot reload the page and take the quiz again.
+				req.session.quizID = 0;
+	
+				// Save the session data.
+				req.session.save();
 			}
 		});
 	});
